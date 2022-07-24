c062a342f5ad44d8f08ed2e45522ef43676902cf
rons9n <rons9n@gmail.com>
2022-07-23 20:28:33 -0400

    NFT components

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as BaseLayout } from "./Layout/BaseLayout";
+export { default as NftItem } from "./nft/item";
+export { default as NftList } from "./nft/list";
