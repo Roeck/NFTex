ab7e282340271a102d800f399ed1b05e4b77633f
rons9n <rons9n@gmail.com>
2022-07-25 19:59:48 -0400

    Links and create page

---
+++ pages/nft/create.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const NftCreate: NextPage = () => {
+
+    return (
+        <BaseLayout>
+            NFT Create
+        </BaseLayout>
+    )
+}
+
+export default NftCreate;
