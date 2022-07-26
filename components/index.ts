e359d5638bd12e5832bfa3dad98f02fe3f9aeea9
rons9n <rons9n@gmail.com>
2022-07-26 15:27:10 -0400

    Active link

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as BaseLayout } from "./Layout/BaseLayout";
-export { default as NftItem } from "./nft/item";
-export { default as NftList } from "./nft/list";
+export { default as NftItem } from "./NFT/item";
+export { default as NftList } from "./NFT/list";
+export { default as ActiveLink } from "./Link";
