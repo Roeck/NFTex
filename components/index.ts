0d5aa447e8ce7218f4fc4f6d44b4271b885a353e
rons9n <rons9n@gmail.com>
2022-07-27 08:27:44 -0400

    Create page layout

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as BaseLayout } from "./Layout/BaseLayout";
 export { default as NftItem } from "./NFT/item";
 export { default as NftList } from "./NFT/list";
-export { default as ActiveLink } from "./Link";
+export { default as ActiveLink } from "./Link/index";
