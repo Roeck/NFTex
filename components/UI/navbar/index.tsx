a01fcf5e195a6e6a86d8f739c0cb9459f533844b
rons9n <rons9n@gmail.com>
2022-08-01 03:56:38 -0400

    Use network

--- components/UI/navbar/index.tsx
+++ components/UI/navbar/index.tsx
@@ -1,7 +1,6 @@
-import { Disclosure, Menu } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
-import { useAccount } from '@hooks/web3';
-import Link from 'next/link';
+import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { useAccount, useNetwork } from '@hooks/web3';
 import ActiveLink from '../link';
 import Walletbar from './Walletbar';
 
@@ -16,6 +15,9 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
     const { account } = useAccount();
+    const { network } = useNetwork();
+
+    console.log(network.data);
 
     return (
         <Disclosure as="nav" className="bg-gray-800">
