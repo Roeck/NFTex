c7cab39259936affc0a34c8149139cf9e281cbad
rons9n <rons9n@gmail.com>
2022-08-01 08:58:16 -0400

    Update loading and undefined Ethereum

--- components/Hooks/web3/useNetwork.ts
+++ components/Hooks/web3/useNetwork.ts
@@ -46,6 +46,6 @@ export const hookFactory: NetworkHookFactory = ({ provider, isLoading }) => () =
         isValidating,
         targetNetwork,
         isSupported: data === targetNetwork,
-        isLoading: isLoading || isValidating,
+        isLoading: isLoading as boolean,
     };
 }
